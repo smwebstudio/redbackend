032586f570cb611704d7f6556471a0fd3855d53e
smweb <sm.programming@gmail.com>
2022-10-24 19:55:00 +0400

    Spatie permission models added. Backpack pro/devtools added.

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,2 +1,6 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+@includeWhen(class_exists(\Backpack\DevTools\DevToolsServiceProvider::class), 'backpack.devtools::buttons.sidebar_item')
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-building-type') }}"><i class="nav-icon la la-question"></i> C building types</a></li>
