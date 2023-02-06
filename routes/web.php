89bbc843002d1250fb90de69898ddbcb25a9e04b
smweb <sm.programming@gmail.com>
2023-02-06 20:58:25 +0400

    Redirected to /admin by default

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return redirect('/admin');
 });
 
 require __DIR__.'/auth.php';
