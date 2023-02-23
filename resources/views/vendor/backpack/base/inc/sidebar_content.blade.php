572e5c49d08879a1a5e8a48b467d8cff4ca60c63
smweb <sm.programming@gmail.com>
2023-02-23 12:15:20 +0400

    Sidebar updated

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -3,9 +3,11 @@
 
 @includeWhen(class_exists(\Backpack\DevTools\DevToolsServiceProvider::class), 'backpack.devtools::buttons.sidebar_item')
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('estate') }}"><i class="nav-icon la la-home"></i> Estates</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('estate') }}"><i class="nav-icon la la-home"></i>Անշարժ Գույք</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('contact') }}"><i class="nav-icon la la-user"></i>Հաճախորդներ</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('messages') }}"><i class="nav-icon la la-envelope"></i>Նամակներ</a></li>
 <li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-bars"></i> Locations</a>
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon las la-search-location"></i>Տարածաշրջան</a>
     <ul class="nav-dropdown-items">
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-location-country') }}"><i class="nav-icon la la-cog"></i> Countries</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-location-province') }}"><i class="nav-icon la la-cog"></i> Provinces</a></li>
@@ -14,16 +16,10 @@
     </ul>
 </li>
 
-<li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-bars"></i> Estate settings</a>
-    <ul class="nav-dropdown-items">
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-building-type') }}"><i class="nav-icon la la-cog"></i> Building types</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-building-project-type') }}"><i class="nav-icon la la-cog"></i> Building project types</a></li>
-    </ul>
-</li>
+
 
 <li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="las la-bars"></i> User management</a>
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-user-alt"></i>Օգտատերեր</a>
     <ul class="nav-dropdown-items">
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-cog"></i> Users</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon la la-cog"></i> Permissions</a></li>
@@ -31,11 +27,26 @@
     </ul>
 </li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('contact') }}"><i class="nav-icon la la-question"></i> Contacts</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-contact-type') }}"><i class="nav-icon la la-question"></i> C contact types</a></li>
 
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('article') }}"><i class="nav-icon la la-question"></i> Articles</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-article-type') }}"><i class="nav-icon la la-question"></i> Article types</a></li>
 
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('estate-option-type') }}"><i class="nav-icon la la-question"></i> Estate option types</a></li>
+
+<li class="nav-item mt-3"><a class="nav-link" href="{{ backpack_url('article') }}"><i class="nav-icon la la-pen-alt"></i> Բլոգ</a></li>
+
+<li class="nav-item nav-dropdown mt-3">
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon las la-cogs"></i>Կարգավորումներ</a>
+    <ul class="nav-dropdown-items">
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-building-type') }}"><i class="nav-icon la la-cog"></i>Շենքի տեսակ</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-building-project-type') }}"><i class="nav-icon la la-cog"></i>Շենքի նախագիծ</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('cc') }}"><i class="nav-icon la la-cog"></i>Բակի բարեկարգում</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('cc') }}"><i class="nav-icon la la-cog"></i>Ներքին հարդարում</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('cc') }}"><i class="nav-icon la la-cog"></i>Մակերես</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('cc') }}"><i class="nav-icon la la-cog"></i>Հեռ. հասարակ. օբյեկտներից</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('cc') }}"><i class="nav-icon la la-cog"></i>Շենքի</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-article-type') }}"><i class="nav-icon la la-question"></i> Article types</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('estate-option-type') }}"><i class="nav-icon la la-question"></i> Estate option types</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('c-contact-type') }}"><i class="nav-icon la la-question"></i> C contact types</a></li>
+    </ul>
+</li>
+
+
